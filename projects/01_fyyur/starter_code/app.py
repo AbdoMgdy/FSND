92e042af2c7609342391850a70ffce4cb9210086
AbdoMgdy <abdoman46@hotmail.com>
2020-11-05 03:37:39 +0200

    add artista validation and clean code

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -285,78 +285,6 @@ def search_artists():
 def show_artist(artist_id):
     # shows the venue page with the given venue_id
     # TODO: replace with real venue data from the venues table, using venue_id [Done]
-    # data1 = {
-    #     "id": 4,
-    #     "name": "Guns N Petals",
-    #     "genres": ["Rock n Roll"],
-    #     "city": "San Francisco",
-    #     "state": "CA",
-    #     "phone": "326-123-5000",
-    #     "website": "https://www.gunsnpetalsband.com",
-    #     "facebook_link": "https://www.facebook.com/GunsNPetals",
-    #     "seeking_venue": True,
-    #     "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    #     "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-    #     "past_shows": [{
-    #         "venue_id": 1,
-    #         "venue_name": "The Musical Hop",
-    #         "venue_image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-    #         "start_time": "2019-05-21T21:30:00.000Z"
-    #     }],
-    #     "upcoming_shows": [],
-    #     "past_shows_count": 1,
-    #     "upcoming_shows_count": 0,
-    # }
-    # data2 = {
-    #     "id": 5,
-    #     "name": "Matt Quevedo",
-    #     "genres": ["Jazz"],
-    #     "city": "New York",
-    #     "state": "NY",
-    #     "phone": "300-400-5000",
-    #     "facebook_link": "https://www.facebook.com/mattquevedo923251523",
-    #     "seeking_venue": False,
-    #     "image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    #     "past_shows": [{
-    #         "venue_id": 3,
-    #         "venue_name": "Park Square Live Music & Coffee",
-    #         "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-    #         "start_time": "2019-06-15T23:00:00.000Z"
-    #     }],
-    #     "upcoming_shows": [],
-    #     "past_shows_count": 1,
-    #     "upcoming_shows_count": 0,
-    # }
-    # data3 = {
-    #     "id": 6,
-    #     "name": "The Wild Sax Band",
-    #     "genres": ["Jazz", "Classical"],
-    #     "city": "San Francisco",
-    #     "state": "CA",
-    #     "phone": "432-325-5432",
-    #     "seeking_venue": False,
-    #     "image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    #     "past_shows": [],
-    #     "upcoming_shows": [{
-    #         "venue_id": 3,
-    #         "venue_name": "Park Square Live Music & Coffee",
-    #         "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-    #         "start_time": "2035-04-01T20:00:00.000Z"
-    #     }, {
-    #         "venue_id": 3,
-    #         "venue_name": "Park Square Live Music & Coffee",
-    #         "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-    #         "start_time": "2035-04-08T20:00:00.000Z"
-    #     }, {
-    #         "venue_id": 3,
-    #         "venue_name": "Park Square Live Music & Coffee",
-    #         "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-    #         "start_time": "2035-04-15T20:00:00.000Z"
-    #     }],
-    #     "past_shows_count": 0,
-    #     "upcoming_shows_count": 3,
-    # }
-    # data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]
     artist = Artist.query.get(artist_id)
     if artist:
         data = {}
@@ -519,33 +447,35 @@ def create_artist_submission():
     # called upon submitting the new artist listing form
     # TODO: insert form data as a new Venue record in the db, instead [Done]
     # TODO: modify data to be the data object returned from db insertion [Done]
+    form = ArtistForm()
+    if form.validate():
+        data = {}
+        data['name'] = request.form.get('name', '')
+        data['city'] = request.form.get('city', '')
+        data['state'] = request.form.get('state', '')
+        data['genres'] = request.form.get('genres', '')
+        data['facebook_link'] = request.form.get('facebook_link', '')
+        data['phone'] = request.form.get('phone', '')
+        error = False
+        try:
+            artist = Artist(**data)
+            db.session.add(artist)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            error = True
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+        # TODO: on unsuccessful db insert, flash an error instead. [Done]
+        if error:
+            flash('An error occurred. Artist ' + request.form.get('name', '') + ' could not be listed.')
+        # on successful db insert, flash success
+        else:
+            flash('Artist ' + request.form.get('name', '') + ' was successfully listed!')
 
-    data = {}
-    data['name'] = request.form.get('name', '')
-    data['city'] = request.form.get('city', '')
-    data['state'] = request.form.get('state', '')
-    data['genres'] = request.form.get('genres', '')
-    data['facebook_link'] = request.form.get('facebook_link', '')
-    data['phone'] = request.form.get('phone', '')
-    error = False
-    try:
-        artist = Artist(**data)
-        db.session.add(artist)
-        db.session.commit()
-    except:
-        db.session.rollback()
-        error = True
-        print(sys.exc_info())
-    finally:
-        db.session.close()
-    # TODO: on unsuccessful db insert, flash an error instead. [Done]
-    if error:
-        flash('An error occurred. Artist ' + request.form.get('name', '') + ' could not be listed.')
-    # on successful db insert, flash success
-    else:
-        flash('Artist ' + request.form.get('name', '') + ' was successfully listed!')
-
-    return render_template('pages/home.html')
+        return render_template('pages/home.html')
+    return 'Invalid Input', 401
 
 
 #  Shows
