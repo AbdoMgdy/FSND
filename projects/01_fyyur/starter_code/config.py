e7d0d88837b55e0ea52868ba5dcec4be42fe9a04
AbdoMgdy <abdoman46@hotmail.com>
2020-11-05 03:41:36 +0200

    change db ur

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -5,11 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
 DEBUG = True
-
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
+# TODO IMPLEMENT DATABASE URL [Done]
 SQLALCHEMY_DATABASE_URI = "postgres://abdo:root@127.0.0.1:5432/fuyyer"
 
 SECRET_KEY = 'this is a very secret string'
